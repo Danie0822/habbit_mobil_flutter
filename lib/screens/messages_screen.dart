4b408ac478800322936f6c71eaebd7a01f210852
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-08-06 19:10:31 -0600

    Se arreglo bug de mensajeria

--- lib/screens/messages_screen.dart
+++ lib/screens/messages_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: deprecated_member_use, avoid_print, library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 import 'package:habbit_mobil_flutter/common/widgets/cards_chat.dart';
 import 'package:habbit_mobil_flutter/utils/constants/colors.dart';
@@ -7,7 +5,6 @@ import 'package:habbit_mobil_flutter/utils/constants/config.dart';
 import 'package:habbit_mobil_flutter/utils/theme/theme_utils.dart';
 import 'package:habbit_mobil_flutter/data/controlers/message.dart';
 import 'package:habbit_mobil_flutter/common/widgets/search_input.dart';
-import 'package:habbit_mobil_flutter/common/widgets/custom_alert.dart';
 
 class MessagesScreen extends StatefulWidget {
   const MessagesScreen({super.key});
@@ -18,265 +15,246 @@ class MessagesScreen extends StatefulWidget {
 
 class _MessagesScreenState extends State<MessagesScreen> with TickerProviderStateMixin {
   final List<ChatCard> _chatCards = []; // Lista de tarjetas de chat
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>(); // Llave para la lista animada
-  bool _isSearchVisible = false; // Estado de visibilidad del campo de búsqueda
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(milliseconds: 300),
-    vsync: this,
-  ); // Controlador de animación para el campo de búsqueda
-  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
-    begin: const Offset(-1.0, 0.0),
-    end: Offset.zero,
-  ).animate(CurvedAnimation(
-    parent: _controller,
-    curve: Curves.easeInOut,
-  )); // Animación de desplazamiento para el campo de búsqueda
-  String _searchQuery = ''; // Consulta de búsqueda actual
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>(); // Clave para la lista animada
+  bool _isSearchVisible = false; // Estado de visibilidad de la búsqueda
+  bool _isLoading = true; // Estado de carga
+  late final AnimationController _controller; // Controlador de animación
+  late final Animation<Offset> _offsetAnimation; // Animación de desplazamiento
+  String _searchQuery = ''; // Consulta de búsqueda
 
   @override
   void initState() {
     super.initState();
-    _loadMessages(); // Carga los mensajes al inicializar
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(-1.0, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+    _loadMessages(); // Cargar mensajes al inicializar
   }
 
+  // Método para cargar mensajes
   Future<void> _loadMessages() async {
     try {
       final messages = await MessageService().cargarChats(); // Cargar mensajes desde el servicio
-      if (messages.isEmpty) {
-        _showNoConversationsAlert(); // Muestra alerta si no hay conversaciones
-      } else {
-        setState(() {
-          _chatCards.clear();
-          for (var message in messages) {
-            _chatCards.add(ChatCard(
-              idConversacion: message.conversacionId ?? 0,
-              title: message.propertyTitle ?? 'Sin título',
-              name: message.adminName ?? 'Sin nombre',
-              message: message.lastMessage ?? 'Sin mensaje',
-              time: message.time ?? '15:00',
-              imageUrl: message.imageUrl != null ? '${Config.imagen}${message.imageUrl}' : '',
-              isRead: message.readMessage ?? false,
-              isAdmin: message.senderType == 'Administrador',
-            ));
-            _listKey.currentState?.insertItem(_chatCards.length - 1); // Inserta el nuevo elemento en la lista animada
-          }
-        });
+      setState(() {
+        _chatCards.clear(); // Limpiar la lista de tarjetas antes de agregar nuevas
+      });
+      if (messages.isNotEmpty) {
+        for (var message in messages) {
+          _chatCards.add(ChatCard(
+            idConversacion: message.conversacionId ?? 0,
+            title: message.propertyTitle ?? 'Sin título',
+            name: message.adminName ?? 'Sin nombre',
+            message: message.lastMessage ?? 'Sin mensaje',
+            time: message.time ?? '15:00',
+            imageUrl: message.imageUrl != null ? '${Config.imagen}${message.imageUrl}' : '',
+            isRead: message.readMessage ?? false,
+            isAdmin: message.senderType == 'Administrador',
+          ));
+        }
+        for (int i = 0; i < _chatCards.length; i++) {
+          _listKey.currentState?.insertItem(i); // Insertar elementos en la lista animada
+        }
       }
     } catch (e) {
-      print('Error cargando chats: $e'); // Manejo de errores al cargar mensajes
+      print('Error cargando chats: $e'); // Manejo de errores
+    } finally {
+      setState(() {
+        _isLoading = false; // Cambiar el estado de carga
+      });
     }
   }
 
-  void _showNoConversationsAlert() {
-    try {
-      showAlertDialog(
-        'Sin conversaciones',
-        'No has iniciado ninguna conversación todavía.',
-        1,
-        context,
-      ); // Muestra alerta si no hay conversaciones
-    } catch (e) {
-      print('Error mostrando alerta: $e'); // Manejo de errores al mostrar alerta
-    }
-  }
-
+  // Método para alternar la visibilidad de la búsqueda
   void _toggleSearch() {
-    try {
-      setState(() {
-        _isSearchVisible = !_isSearchVisible; // Alterna la visibilidad del campo de búsqueda
-        if (_isSearchVisible) {
-          _controller.forward(); // Muestra el campo de búsqueda con animación
-        } else {
-          _controller.reverse(); // Oculta el campo de búsqueda con animación
-          _searchQuery = '';
-          _refreshMessages(); // Refresca los mensajes al ocultar el campo de búsqueda
-        }
-      });
-    } catch (e) {
-      print('Error alternando búsqueda: $e'); // Manejo de errores al alternar búsqueda
-    }
+    setState(() {
+      _isSearchVisible = !_isSearchVisible;
+      if (_isSearchVisible) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+        _searchQuery = '';
+        _refreshMessages(); // Refrescar mensajes al cerrar la búsqueda
+      }
+    });
   }
 
+  // Método para filtrar mensajes según la consulta de búsqueda
   void _filterMessages(String query) {
-    try {
-      setState(() {
-        _searchQuery = query.toLowerCase(); // Actualiza la consulta de búsqueda
-      });
-    } catch (e) {
-      print('Error filtrando mensajes: $e'); // Manejo de errores al filtrar mensajes
-    }
+    setState(() {
+      _searchQuery = query.toLowerCase();
+    });
   }
 
+  // Método para refrescar los mensajes
   Future<void> _refreshMessages() async {
     try {
+      setState(() {
+        _isLoading = true;
+      });
+      List<Future> futures = [];
       for (int i = _chatCards.length - 1; i >= 0; i--) {
-        _listKey.currentState?.removeItem(
-          i,
-          (context, animation) => _buildAnimatedItem(context, i, animation),
-          duration: const Duration(milliseconds: 300),
-        ); // Elimina elementos de la lista animada
+        futures.add(
+          Future.delayed(const Duration(milliseconds: 100), () {
+            _listKey.currentState?.removeItem(
+              i,
+              (context, animation) => _buildAnimatedItem(context, i, animation),
+              duration: const Duration(milliseconds: 600),
+            );
+          }),
+        );
       }
-      await _loadMessages(); // Recarga los mensajes
+      await Future.wait(futures); // Esperar a que todas las tarjetas sean removidas
+      setState(() {
+        _chatCards.clear();
+      });
+      await _loadMessages(); // Cargar mensajes nuevamente
     } catch (e) {
-      print('Error refrescando mensajes: $e'); // Manejo de errores al refrescar mensajes
+      print('Error refrescando mensajes: $e'); // Manejo de errores
     }
   }
 
   @override
   void dispose() {
-    try {
-      _controller.dispose(); // Libera el controlador de animación
-      super.dispose();
-    } catch (e) {
-      print('Error liberando recursos: $e'); // Manejo de errores al liberar recursos
-    }
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    try {
-      final Color containerMain = ThemeUtils.getColorBasedOnBrightness(
-          context, colorBackGroundMessageContainerLight, almostBlackColor); // Obtiene el color basado en el tema
+    final Color containerMain = ThemeUtils.getColorBasedOnBrightness(
+        context, colorBackGroundMessageContainerLight, almostBlackColor);
 
-      return WillPopScope(
-        onWillPop: () async {
-          try {
-            await _loadMessages(); // Recarga mensajes al regresar
-          } catch (e) {
-            print('Error al regresar: $e'); // Manejo de errores al regresar
-          }
-          return true;
-        },
-        child: Scaffold(
-          backgroundColor: colorBackGroundMessage,
-          body: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _buildHeader(), // Construye la cabecera de la pantalla
-                const SizedBox(height: 20.0),
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: containerMain,
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
-                      child: RefreshIndicator(
-                        onRefresh: _refreshMessages, // Actualiza los mensajes al hacer pull-to-refresh
-                        child: AnimatedList(
-                          key: _listKey,
-                          padding: const EdgeInsets.only(top: 35.0),
-                          initialItemCount: _chatCards.length,
-                          itemBuilder: (context, index, animation) {
-                            return _buildFilteredItem(context, index, animation); // Construye los elementos de la lista filtrados
-                          },
-                        ),
-                      ),
+    return WillPopScope(
+      onWillPop: () async {
+        await _loadMessages(); // Cargar mensajes al salir
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: colorBackGroundMessage,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildHeader(), // Construir encabezado
+              const SizedBox(height: 20.0),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: containerMain,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
-    } catch (e) {
-      print('Error construyendo la pantalla: $e'); // Manejo de errores al construir la pantalla
-      return Container(); // Devuelve un contenedor vacío en caso de error
-    }
-  }
-
-  Widget _buildHeader() {
-    try {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Mensajes',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: whiteColor,
-                  ),
-                ),
-                IconButton(
-                  onPressed: _toggleSearch, // Alterna la visibilidad del campo de búsqueda
-                  icon: AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 300),
-                    transitionBuilder: (Widget child, Animation<double> animation) {
-                      return RotationTransition(
-                        turns: child.key == const ValueKey('search') ? animation : Tween<double>(begin: 1, end: 0.75).animate(animation),
-                        child: FadeTransition(opacity: animation, child: child),
-                      ); // Transición animada para el ícono de búsqueda
-                    },
-                    child: Icon(
-                      _isSearchVisible ? Icons.close : Icons.search,
-                      key: ValueKey(_isSearchVisible ? 'close' : 'search'),
-                      color: whiteColor,
-                      size: 25.0,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                    child: RefreshIndicator(
+                      onRefresh: _refreshMessages, // Refrescar mensajes al arrastrar hacia abajo
+                      child: _isLoading
+                          ? Center(child: CircularProgressIndicator()) // Mostrar indicador de carga
+                          : _chatCards.isEmpty
+                              ? Center(child: Text('No has iniciado ninguna conversación todavía.')) // Mostrar mensaje si no hay conversaciones
+                              : AnimatedList(
+                                  key: _listKey,
+                                  padding: const EdgeInsets.only(top: 35.0),
+                                  initialItemCount: _chatCards.length,
+                                  itemBuilder: (context, index, animation) {
+                                    return _buildFilteredItem(context, index, animation); // Construir elemento filtrado
+                                  },
+                                ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            if (_isSearchVisible)
-              SlideTransition(
-                position: _offsetAnimation, // Animación de deslizamiento para el campo de búsqueda
-                child: SearchInput(
-                  onChanged: _filterMessages, // Filtra los mensajes según la consulta de búsqueda
-                  hintText: 'Buscar por título...',
                 ),
               ),
-          ],
+            ],
+          ),
         ),
-      );
-    } catch (e) {
-      print('Error construyendo la cabecera: $e'); // Manejo de errores al construir la cabecera
-      return Container(); // Devuelve un contenedor vacío en caso de error
-    }
+      ),
+    );
   }
 
+  // Construir encabezado
+  Widget _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Mensajes',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: whiteColor,
+                ),
+              ),
+              IconButton(
+                onPressed: _toggleSearch,
+                icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 600),
+                  transitionBuilder: (Widget child, Animation<double> animation) {
+                    return RotationTransition(
+                      turns: child.key == const ValueKey('search') ? animation : Tween<double>(begin: 1, end: 0.75).animate(animation),
+                      child: FadeTransition(opacity: animation, child: child),
+                    );
+                  },
+                  child: Icon(
+                    _isSearchVisible ? Icons.close : Icons.search,
+                    key: ValueKey(_isSearchVisible ? 'close' : 'search'),
+                    color: whiteColor,
+                    size: 25.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          if (_isSearchVisible)
+            SlideTransition(
+              position: _offsetAnimation,
+              child: SearchInput(
+                onChanged: _filterMessages,
+                hintText: 'Buscar por título...',
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  // Construir elemento animado
   Widget _buildAnimatedItem(BuildContext context, int index, Animation<double> animation) {
-    try {
-      return FadeTransition(
-        opacity: animation,
-        child: SlideTransition(
-          position: Tween<Offset>(
-            begin: const Offset(-1, 0),
-            end: Offset.zero,
-          ).animate(animation), // Transición de deslizamiento para las tarjetas de chat
-          child: _chatCards[index], // Construye la tarjeta de chat
-        ),
-      );
-    } catch (e) {
-      print('Error construyendo el elemento animado: $e'); // Manejo de errores al construir el elemento animado
-      return Container(); // Devuelve un contenedor vacío en caso de error
-    }
+    return FadeTransition(
+      opacity: animation,
+      child: SlideTransition(
+        position: Tween<Offset>(
+          begin: const Offset(-1, 0),
+          end: Offset.zero,
+        ).animate(animation),
+        child: _chatCards[index],
+      ),
+    );
   }
 
+  // Construir elemento filtrado
   Widget _buildFilteredItem(BuildContext context, int index, Animation<double> animation) {
-    try {
-      if (_searchQuery.isEmpty || _chatCards[index].title.toLowerCase().contains(_searchQuery)) {
-        return _buildAnimatedItem(context, index, animation); // Muestra las tarjetas filtradas
-      } else {
-        return Container(); // Oculta el elemento si no coincide con la búsqueda
-      }
-    } catch (e) {
-      print('Error construyendo el elemento filtrado: $e'); // Manejo de errores al construir el elemento filtrado
-      return Container(); // Devuelve un contenedor vacío en caso de error
+    if (_searchQuery.isEmpty || _chatCards[index].title.toLowerCase().contains(_searchQuery)) {
+      return _buildAnimatedItem(context, index, animation);
+    } else {
+      return Container();
     }
   }
 }
