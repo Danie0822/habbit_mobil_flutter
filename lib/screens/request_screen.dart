fa622b286b755ccf66bb01de225afb391b2be9c8
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-09-15 21:43:54 -0600

    Cambios de request_screen

--- lib/screens/request_screen.dart
+++ lib/screens/request_screen.dart
@@ -6,6 +6,8 @@ import 'package:habbit_mobil_flutter/common/widgets/detail_request.dart';
 import 'package:habbit_mobil_flutter/common/widgets/header_screen.dart';
 import 'package:habbit_mobil_flutter/data/controlers/request_controlers.dart';
 import 'package:habbit_mobil_flutter/data/models/request_model.dart';
+import 'package:habbit_mobil_flutter/utils/constants/colors.dart';
+import 'package:habbit_mobil_flutter/utils/theme/theme_utils.dart';
 
 class RequestsScreen extends StatefulWidget {
   const RequestsScreen({super.key});
@@ -14,26 +16,21 @@ class RequestsScreen extends StatefulWidget {
   State<RequestsScreen> createState() => _RequestsScreenState();
 }
 
-class _RequestsScreenState extends State<RequestsScreen> with SingleTickerProviderStateMixin {
-  // Variables de listas de solicitudes
+class _RequestsScreenState extends State<RequestsScreen>
+    with SingleTickerProviderStateMixin {
   List<RequestModel> requestsData = [];
   List<RequestModel> filteredRequests = [];
-  // Variables de estado
   bool isLoading = true;
-  // Variables de error
   String? errorMessage;
-  // Variables de búsqueda
   bool isSearchVisible = false;
-  // Variables de busquedas 
   String searchQuery = '';
   late AnimationController _controller;
   late Animation<Offset> _offsetAnimation;
- // Inicializar las solicitudes
+
   @override
   void initState() {
     super.initState();
     _loadRequests();
-    // Inicializar la animación del buscador
     _controller = AnimationController(
       duration: const Duration(milliseconds: 500),
       vsync: this,
@@ -46,7 +43,7 @@ class _RequestsScreenState extends State<RequestsScreen> with SingleTickerProvid
       curve: Curves.easeInOut,
     ));
   }
-  // Cargar las solicitudes
+
   Future<void> _loadRequests() async {
     try {
       setState(() {
@@ -55,7 +52,7 @@ class _RequestsScreenState extends State<RequestsScreen> with SingleTickerProvid
       final loadedRequests = await RequestService().cargarRequest();
       setState(() {
         requestsData = loadedRequests;
-        filteredRequests = requestsData; // Inicialmente, todas las solicitudes
+        filteredRequests = requestsData;
         isLoading = false;
       });
     } catch (error) {
@@ -64,40 +61,43 @@ class _RequestsScreenState extends State<RequestsScreen> with SingleTickerProvid
         isLoading = false;
       });
     }
-  } 
-  // Función para mostrar u ocultar el buscador
+  }
+
   void _toggleSearch() {
     setState(() {
       isSearchVisible = !isSearchVisible;
       if (isSearchVisible) {
-        _controller.forward(); // Mostrar el buscador
+        _controller.forward();
       } else {
-        _controller.reverse(); // Ocultar el buscador
+        _controller.reverse();
         setState(() {
-          filteredRequests = requestsData; // Resetear la lista al cerrar el buscador
+          filteredRequests = requestsData;
         });
       }
     });
   }
-  // Función para filtrar las solicitudes
+
   void _onSearchChanged(String query) {
     setState(() {
       searchQuery = query;
       filteredRequests = requestsData
           .where((request) => request.tituloSolicitud
               .toLowerCase()
-              .contains(query.toLowerCase())) // Filtrar solicitudes
+              .contains(query.toLowerCase()))
           .toList();
     });
   }
-  // Construir la pantalla de solicitudes el diseño 
+
   @override
   Widget build(BuildContext context) {
+    final Color containerMain = ThemeUtils.getColorBasedOnBrightness(
+        context, colorBackGroundMessageContainerLight, almostBlackColor);
+
     return Scaffold(
+      backgroundColor: colorBackGroundMessage,
       body: Column(
         children: [
-          const SizedBox(height: 40),
-          // Encabezado de la pantalla
+          const SizedBox(height: 20),
           HeaderScreen(
             isSearchVisible: isSearchVisible,
             onSearchToggle: _toggleSearch,
@@ -106,20 +106,45 @@ class _RequestsScreenState extends State<RequestsScreen> with SingleTickerProvid
             hintTextt: 'Buscar solicitudes...',
             titleHeader: 'Solicitudes',
           ),
-          // Cuerpo de la pantalla
           Expanded(
-            child: isLoading
-                ? const Center(child: CircularProgressIndicator())
-                : errorMessage != null
-                    ? Center(child: Text(errorMessage!))
-                    : RefreshIndicator(
-                        onRefresh: _loadRequests,
-                        child: ListRequest(requestsData: filteredRequests), // Mostrar la lista filtrada
-                      ),
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: containerMain,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+                child: isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : errorMessage != null
+                        ? Center(
+                            child: Text(
+                              errorMessage!,
+                              textAlign: TextAlign
+                                  .center, // Asegura que el texto esté centrado horizontalmente
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        : RefreshIndicator(
+                            onRefresh: _loadRequests,
+                            child: ListRequest(requestsData: filteredRequests),
+                          ),
+              ),
+            ),
           ),
         ],
       ),
-      // Botón flotante para agregar una nueva solicitud
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           context.go('/newRequest');
@@ -133,14 +158,14 @@ class _RequestsScreenState extends State<RequestsScreen> with SingleTickerProvid
       ),
     );
   }
-  // Liberar recursos
+
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
 }
-// Clase para mostrar la lista de solicitudes
+
 class ListRequest extends StatelessWidget {
   const ListRequest({super.key, required this.requestsData});
 
@@ -148,16 +173,14 @@ class ListRequest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder( // Construir la lista de solicitudes
+    return ListView.builder(
       padding: const EdgeInsets.all(8.0),
       itemCount: requestsData.length,
       itemBuilder: (context, index) {
         final request = requestsData[index];
-        // Widget para deslizar y eliminar una solicitud
         return Dismissible(
           key: ValueKey(request.idSolicitud),
-          direction: DismissDirection
-              .endToStart, // Permite deslizar de derecha a izquierda
+          direction: DismissDirection.endToStart,
           background: Container(
             color: Colors.red,
             alignment: Alignment.centerRight,
@@ -170,7 +193,6 @@ class ListRequest extends StatelessWidget {
           ),
           onDismissed: (direction) {},
           confirmDismiss: (direction) async {
-            // Confirmar la acción de eliminar
             return await showDialog(
               context: context,
               builder: (context) => AlertDialog(
@@ -180,17 +202,14 @@ class ListRequest extends StatelessWidget {
                 actions: [
                   TextButton(
                     onPressed: () {
-                      Navigator.of(context)
-                          .pop(false); // No hace nada y cancela
+                      Navigator.of(context).pop(false);
                     },
                     child: const Text('Cancelar'),
                   ),
                   TextButton(
                     onPressed: () {
-                      Navigator.of(context)
-                          .pop(true); // Confirma la eliminación
-                      _deleteRequest(request.idSolicitud,
-                          context); // Llama a la función de eliminación
+                      Navigator.of(context).pop(true);
+                      _deleteRequest(request.idSolicitud, context);
                     },
                     child: const Text('Eliminar'),
                   ),
@@ -209,7 +228,7 @@ class ListRequest extends StatelessWidget {
               ),
               elevation: 4,
               child: RequestsCards(
-                    request: request,
+                request: request,
               ),
             ),
           ),
@@ -217,12 +236,12 @@ class ListRequest extends StatelessWidget {
       },
     );
   }
-  // Función para eliminar una solicitud
+
   void _deleteRequest(int idSolicitud, BuildContext context) {
     RequestService().requestDelete(idSolicitud).then((value) {
       if (value == 1) {
         showAlertDialog(
-            'Éxito', 'Se ha eliminado la solicitud exitosamente ', 3, context);
+            'Éxito', 'Se ha eliminado la solicitud exitosamente', 3, context);
       } else {
         showAlertDialog(
             'Error', 'No se ha podido eliminar la solicitud', 2, context);
