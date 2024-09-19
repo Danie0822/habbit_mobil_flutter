b83ef05c7e771657fab9f6e1fa102ec0f934f0be
Danie0822 <20190652@ricaldone.edu.sv>
2024-09-19 12:09:26 -0600

    cambios de main

--- lib/data/services/api_service.dart
+++ lib/data/services/api_service.dart
@@ -2,52 +2,99 @@ import 'dart:convert';
 import 'package:habbit_mobil_flutter/data/services/storage_service.dart';
 import 'package:habbit_mobil_flutter/utils/constants/config.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class ApiService {
   // Variable para controlar si se está realizando una petición
   static bool isFetchingData = false;
+
+  // Cola de peticiones
+  static List<Function> requestQueue = [];
+
   // Método para obtener datos
   static Future<Map<String, dynamic>> fetchData(String endpoint) async {
-    return await _fetchDataWithOptions(endpoint, {
-      'headers': {
-        'Authorization': 'Bearer ${await StorageService.getToken()}'
-      }
+    return await _queueRequest(() async {
+      return await _fetchDataWithOptions(endpoint, {
+        'headers': {
+          'Authorization': 'Bearer ${await StorageService.getToken()}'
+        }
+      });
     });
   }
+
   // Método para enviar datos
   static Future<Map<String, dynamic>> sendData(String endpoint, String method, Map<String, dynamic>? formData) async {
-    return await _fetchDataWithOptions(endpoint, {
-      'method': method,
-      'headers': {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${await StorageService.getToken()}'
-      },
-      'body': formData != null ? jsonEncode(formData) : null,
+    return await _queueRequest(() async {
+      return await _fetchDataWithOptions(endpoint, {
+        'method': method,
+        'headers': {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ${await StorageService.getToken()}'
+        },
+        'body': formData != null ? jsonEncode(formData) : null,
+      });
     });
   }
+
   // Método para enviar datos con API Key
   static Future<Map<String, dynamic>> sendDataApiKey(String endpoint, String method, Map<String, dynamic>? formData) async {
-    return await _fetchDataWithOptions(endpoint, {
-      'method': method,
-      'headers': {
-        'Content-Type': 'application/json',
-        'x-api-key': Config.apiKey
-      },
-      'body': formData != null ? jsonEncode(formData) : null,
+    return await _queueRequest(() async {
+      return await _fetchDataWithOptions(endpoint, {
+        'method': method,
+        'headers': {
+          'Content-Type': 'application/json',
+          'x-api-key': Config.apiKey
+        },
+        'body': formData != null ? jsonEncode(formData) : null,
+      });
     });
   }
+
   // Método para eliminar datos
   static Future<Map<String, dynamic>> deleteData(String endpoint) async {
-    return await _fetchDataWithOptions(endpoint, {
-      'method': 'DELETE',
-      'headers': {
-        'Authorization': 'Bearer ${await StorageService.getToken()}'
-      }
+    return await _queueRequest(() async {
+      return await _fetchDataWithOptions(endpoint, {
+        'method': 'DELETE',
+        'headers': {
+          'Authorization': 'Bearer ${await StorageService.getToken()}'
+        }
+      });
     });
   }
+
+  // Método que añade la petición a la cola
+  static Future<Map<String, dynamic>> _queueRequest(Future<Map<String, dynamic>> Function() request) async {
+    // Agregamos la petición a la cola
+    final completer = Completer<Map<String, dynamic>>();
+    requestQueue.add(() => request().then(completer.complete).catchError(completer.completeError));
+
+    // Si no se está ejecutando ninguna petición, ejecutamos la primera
+    if (!isFetchingData) {
+      _processQueue();
+    }
+
+    return completer.future;
+  }
+
+  // Método para procesar la cola de peticiones
+  static Future<void> _processQueue() async {
+    if (requestQueue.isEmpty) return;
+
+    isFetchingData = true;
+
+    // Ejecutamos la primera petición en la cola
+    final currentRequest = requestQueue.removeAt(0);
+    await currentRequest();
+
+    // Continuamos con la siguiente petición
+    isFetchingData = false;
+    if (requestQueue.isNotEmpty) {
+      _processQueue();
+    }
+  }
+
   // Método para manejar las peticiones
   static Future<Map<String, dynamic>> _fetchDataWithOptions(String endpoint, Map<String, dynamic> options) async {
-    await _checkFetchingStatus();
     try {
       final uri = Uri.parse('${Config.baseURL}$endpoint');
       final response = await _makeRequest(uri, options);
@@ -55,10 +102,9 @@ class ApiService {
       return _handleResponse(response);
     } catch (error) {
       return _handleError(error);
-    } finally {
-      isFetchingData = false;
-    }
+    } 
   }
+
   // Método para realizar la petición
   static Future<http.Response> _makeRequest(Uri uri, Map<String, dynamic> options) {
     final method = options['method'] ?? 'GET';
@@ -79,18 +125,13 @@ class ApiService {
         return http.get(uri, headers: headers);
     }
   }
-  // Método para verificar si se está realizando una petición
-  static Future<void> _checkFetchingStatus() async {
-    if (isFetchingData) {
-      throw Exception("Otra operación está en curso. Por favor, espere.");
-    }
-    isFetchingData = true;
-  }
+
   // Método para manejar la respuesta
   static Map<String, dynamic> _handleResponse(http.Response response) {
     final data = jsonDecode(response.body);
     return {'success': response.statusCode == 200, 'data': data};
   }
+
   // Método para manejar los errores
   static Map<String, dynamic> _handleError(Object error) {
     // ignore: avoid_print
