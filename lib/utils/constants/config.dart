2f2949a4f18c4f4c8014ad068d8a3f093ab6109e
Danie0822 <106714398+Danie0822@users.noreply.github.com>
2024-11-09 13:58:26 -0600

    Update config.dart

--- lib/utils/constants/config.dart
+++ lib/utils/constants/config.dart
@@ -1,11 +1,11 @@
 class Config {
   // Constantes de configuración
   // URL base de la API
-  static const String url = "https://habbitapi-b0b8f90f069e.herokuapp.com";
+  static const String url = "http://172.24.240.1:4000";
   // URL base de la APIs
   static const String baseURL = "$url/api";
   // URL base de las imágenes
   static const String imagen = "$url/";
   // Clave de la API
-  static const String apiKey = "7gFtu8Lz2BqAnW3PmDhAeSxGz";
+  static const String apiKey = "7gFtu8Lz2BqAnW3PmDhAeSKSWNS";
 }
